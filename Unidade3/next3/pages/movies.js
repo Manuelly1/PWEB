409ba19b36f695994f558ff07fb99068a3633917
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-11-26 09:41:06 -0300

    update (tá com um erro ainda)

--- Unidade3/next3/pages/movies.js
+++ Unidade3/next3/pages/movies.js
@@ -1,90 +1,65 @@
-import React, { useState } from 'react';
-import styles from '../styles/movies.module.css';
+import { useState } from "react";
+import styles from "../styles/movies.module.css";
 
-export default function Movies() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+export default function Movies({ initialData }) {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [data, setData] = useState(initialData);
 
-  const data = {
-    "Search": [
-      {
-        "Title": "Bagdad Café",
-        "Year": "1987",
-        "imdbID": "tt0095801",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNmUwZGZhZTktNGNmOS00NDA0LWEzYWUtZjE2ODA5OTM2OTc2XkEyXkFqcGdeQXVyMTQ3Njg3MQ@@._V1_SX300.jpg"
-      },
-      {
-        "Title": "The Thief of Bagdad",
-        "Year": "1940",
-        "imdbID": "tt0033152",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZWFhYjg4NTEtY2IzMS00YTc2LTg1NGUtMTEzNDBlZDIxZTk3XkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_SX300.jpg"
-      },
-      {
-        "Title": "Bagdad",
-        "Year": "1949",
-        "imdbID": "tt0041149",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZjAxYTJmMmItYzZhMS00OWMwLTkxMjEtZjVmNmM3ZGRlZTljXkEyXkFqcGdeQXVyNTk1MTk0MDI@._V1_SX300.jpg"
-      },
-      {
-        "Title": "Bowery to Bagdad",
-        "Year": "1955",
-        "imdbID": "tt0044388",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZTMwMTUyZTktOTk4OC00ZGY2LWFmMzAtYjkxMWMxNjdmOTg4XkEyXkFqcGdeQXVyNDY3MzU2MDM@._V1_SX300.jpg"
-      },
-      {
-        "Title": "Babes in Bagdad",
-        "Year": "1952",
-        "imdbID": "tt0044389",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BYjBmNmRlNzAtZDNjYi00ZjQ0LTljYTktZGUxOGNhMjFhZWI1XkEyXkFqcGdeQXVyMTE2NzA0Ng@@._V1_SX300.jpg"
-      }
-    ],
-    "totalResults": "59",
-    "Response": "True"
-  };
-
-  const searchByKeyword = (keyword) => {
-    const results = data.Search.filter(movie => movie.Title.toLowerCase().includes(keyword.toLowerCase()));
-    setSearchResults(results);
-  };
-
-  const handleSearch = () => {
-    searchByKeyword(searchTerm);
+  const handleSearch = async () => {
+    const res = await fetch(
+      `https://www.omdbapi.com/?i=tt3896198&apikey=a1705ee9&s=${searchQuery}`
+    );
+    const newData = await res.json();
+    setData(newData);
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.searchContainer}>
+    <div className={styles.pageContainer}>
+      <div className={styles.forms}>
         <input
           type="text"
-          placeholder="Digite uma palavra-chave"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className={styles.inputSearch}
+          placeholder="Digite a palavra-chave:"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button onClick={handleSearch} className={styles.buttonSearch}>
+        <button className={styles.searchButton} onClick={handleSearch}>
           Pesquisar
         </button>
       </div>
-
-      {searchResults.length > 0 ? (
-        <div className={styles.movieContainer}>
-          {searchResults.map((movie) => (
-            <div key={movie.imdbID} className={styles.movieCard}>
-              <h2 className={styles.movieTitle}>
-                {movie.Title} ({movie.Year})
-              </h2>
-              <img src={movie.Poster} alt={movie.Title} className={styles.poster} />
+      <div className={styles.moviesContainer}>
+        {data.Search &&
+          data.Search.map((m) => (
+            <div key={m.imdbID} className={styles.movieCard}>
+              <a
+                href={`https://www.imdb.com/title/${m.imdbID}/`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={m.Poster} alt={m.Title} />
+              </a>
+              <div className={styles.movieText}>
+                {m.Title} --- {m.Year}
+              </div>
             </div>
           ))}
-        </div>
-      ) : (
-        <p>Nenhum resultado encontrado.</p>
-      )}
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const searchQuery = query.q || 'bagdad';
+  
+  const res = await fetch(
+    `https://www.omdbapi.com/?i=tt3896198&apikey=a1705ee9&s=${searchQuery}`
+  );  
+  
+  const data = await res.json();
+
+  return {
+    props: {
+      initialData: data,
+    },
+  };
 }
